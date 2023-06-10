4b94beacc3117ef4f9a30dd3c07b918d704c2fa6
gozali <gozaliatamimi0@gmail.com>
2023-06-10 19:18:25 +0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic',
+  appName: 'Ionic-UAS',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
